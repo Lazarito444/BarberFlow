23adc8e992d00b1be2c2f45c1c1ab4b5079ddf0b
Ariel Lázaro <ariellazaro444@gmail.com>
2026-01-06 22:12:36 -0400

    Add base entity class

---
+++ BarberFlow.Core.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace BarberFlow.Core.Domain.Entities;
+
+public abstract class BaseEntity
+{
+    public Guid Id { get; set; }
+    public DateTime CreatedAtUtc { get; set; }
+    public DateTime? UpdatedAtUtc { get; set; }
+    public bool IsDeleted { get; set; }
+}
