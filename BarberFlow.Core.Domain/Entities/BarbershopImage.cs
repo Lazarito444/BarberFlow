e246308e058fa1cb0428588ee38ee8c388d31d95
Ariel Lázaro <ariellazaro444@gmail.com>
2026-01-14 21:57:34 -0400

    Add BarbershopImage entity

---
+++ BarberFlow.Core.Domain/Entities/BarbershopImage.cs
@@ -0,0 +1,9 @@
+﻿namespace BarberFlow.Core.Domain.Entities;
+
+public class BarbershopImage : BaseEntity
+{
+    public Guid BarbershopId { get; set; }
+    public Barbershop Barbershop { get; set; } = null!;
+    public string ImageUrl { get; set; } = string.Empty;
+    public bool IsPrimary { get; set; }
+}
