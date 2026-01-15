4f645cb41f0569a124eb4b461a1e0829c01bc62b
Ariel Lázaro <ariellazaro444@gmail.com>
2026-01-14 21:53:53 -0400

    Add WorkSchedule, ScheduleException entities

---
+++ BarberFlow.Core.Domain/Entities/ScheduleException.cs
@@ -0,0 +1,12 @@
+﻿namespace BarberFlow.Core.Domain.Entities;
+
+public class ScheduleException : BaseEntity
+{
+    public Guid BarbershopId { get; set; }
+    public Barbershop Barbershop { get; set; } = null!;
+    public DateOnly Date { get; set; }
+    public TimeOnly? StartTime { get; set; }
+    public TimeOnly? EndTime { get; set; }
+    public bool IsClosed { get; set; }
+    public string? Reason { get; set; } = string.Empty;
+}
---
+++ BarberFlow.Core.Domain/Entities/WorkSchedule.cs
@@ -0,0 +1,11 @@
+﻿namespace BarberFlow.Core.Domain.Entities;
+
+public class WorkSchedule : BaseEntity
+{
+    public Guid BarbershopId { get; set; }
+    public Barbershop Barbershop { get; set; } = null!;
+    public DayOfWeek DayOfWeek { get; set; }
+    public TimeOnly StartTime { get; set; }
+    public TimeOnly EndTime { get; set; }
+    public bool IsActive { get; set; }
+}
