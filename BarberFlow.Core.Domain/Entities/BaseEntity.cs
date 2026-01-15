3f9bcab1b843fc153269127a0b5eb024d585199c
Ariel Lázaro <ariellazaro444@gmail.com>
2026-01-14 21:47:37 -0400

    Update BaseEntity to include CreatedBy & UpdatedBy properties

--- BarberFlow.Core.Domain/Entities/BaseEntity.cs
+++ BarberFlow.Core.Domain/Entities/BaseEntity.cs
@@ -4,6 +4,8 @@ public abstract class BaseEntity
 {
     public Guid Id { get; set; }
     public DateTime CreatedAtUtc { get; set; }
-    public DateTime? UpdatedAtUtc { get; set; }
+    public Guid CreatedBy { get; set; }
+    public DateTime UpdatedAtUtc { get; set; }
+    public Guid UpdatedBy { get; set; }
     public bool IsDeleted { get; set; }
 }
