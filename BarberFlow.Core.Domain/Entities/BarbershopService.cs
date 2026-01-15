077807338b7f6595c52358791087943ca4ccf9ae
Ariel Lázaro <ariellazaro444@gmail.com>
2026-01-14 21:47:50 -0400

    Add BarbershopService entity

---
+++ BarberFlow.Core.Domain/Entities/BarbershopService.cs
@@ -0,0 +1,12 @@
+﻿namespace BarberFlow.Core.Domain.Entities;
+
+public class BarbershopService : BaseEntity
+{
+    public Guid BarbershopId { get; set; }
+    public Barbershop Barbershop { get; set; } = null!;
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+    public int DurationInMinutes { get; set; }
+    public bool IsActive { get; set; }
+}
