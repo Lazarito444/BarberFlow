1fb2328ffafe5508189e7bdb0bb6532661454595
Ariel Lázaro <ariellazaro444@gmail.com>
2026-01-14 21:54:10 -0400

    Add Appointment entity & AppointmentStatus enum

---
+++ BarberFlow.Core.Domain/Entities/Appointment.cs
@@ -0,0 +1,14 @@
+﻿namespace BarberFlow.Core.Domain.Entities;
+
+public class Appointment : BaseEntity
+{
+    public Guid BarbershopId { get; set; }
+    public Barbershop Barbershop { get; set; } = null!;
+    public Guid ServiceId { get; set; }
+    public BarbershopService Service { get; set; } = null!;
+    public Guid CustomerId { get; set; }
+    public DateOnly Date { get; set; }
+    public TimeOnly StartTime { get; set; }
+    public TimeOnly EndTime { get; set; }
+    public AppointmentStatus Status { get; set; }
+}
---
+++ BarberFlow.Core.Domain/Enums/AppointmentStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace BarberFlow.Core.Domain.Enums;
+
+public enum AppointmentStatus
+{
+    Pending = 0,
+    Accepted = 1,
+    Rejected = 2,
+    Canceled = 3,
+    Completed = 4
+}
