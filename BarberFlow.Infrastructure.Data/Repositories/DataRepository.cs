b002f9bdf211aab4fcb653ebe1afbe1adf777532
Ariel Lázaro <ariellazaro444@gmail.com>
2026-01-16 22:08:30 -0400

    Add CountBySpecification, HardDelete, GetById methods in DataRepository

--- BarberFlow.Infrastructure.Data/Repositories/DataRepository.cs
+++ BarberFlow.Infrastructure.Data/Repositories/DataRepository.cs
@@ -1,4 +1,7 @@
-﻿namespace BarberFlow.Infrastructure.Data.Repositories;
+﻿using BarberFlow.Core.Domain.OperationResults;
+using Microsoft.EntityFrameworkCore;
+
+namespace BarberFlow.Infrastructure.Data.Repositories;
 
 public class DataRepository<TEntity>
     where TEntity : class
@@ -10,8 +13,44 @@ public class DataRepository<TEntity>
         _context = context;
     }
 
-    public List<TEntity> GetPage()
+    public async Task<int> CountBySpecification(Specification<TEntity> specification, CancellationToken ct = default)
     {
-        return _context.Set<TEntity>().ToList();
+        ct.ThrowIfCancellationRequested();
+
+        IQueryable<TEntity> aggregate = specification.Includes
+            .Aggregate(_context.Set<TEntity>().AsQueryable(), (current, include)
+            => current.Include(include));
+
+        return await aggregate.CountAsync(specification.Criteria, ct);
+    }
+
+    public async Task HardDelete(TEntity entity, CancellationToken ct = default)
+    {
+        ct.ThrowIfCancellationRequested();
+
+        _context.Set<TEntity>().Remove(entity);
+        await _context.SaveChangesAsync(ct);
+    }
+
+    public async Task HardDelete(Guid id, CancellationToken ct = default)
+    {
+        ct.ThrowIfCancellationRequested();
+
+        TEntity? entity = await _context.Set<TEntity>().FindAsync([id], cancellationToken: ct);
+
+        if (entity is null) return;
+
+        _context.Set<TEntity>().Remove(entity);
+        await _context.SaveChangesAsync(ct);
+    }
+
+    public async Task<TEntity?> GetById(Guid id)
+    {
+        return await _context.Set<TEntity>().FindAsync(id);
+    }
+
+    public async Task<List<TEntity>> GetPage()
+    {
+        return await _context.Set<TEntity>().ToListAsync();
     }
 }
